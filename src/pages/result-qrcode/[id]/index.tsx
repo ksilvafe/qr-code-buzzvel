e9bda74e8c497a7b7218a88e069938ea92b3957d
Kevin Ferreira <42346207+ksilvafe@users.noreply.github.com>
2023-03-17 10:56:28 -0300

    correction type

--- src/pages/result-qrcode/[id]/index.tsx
+++ src/pages/result-qrcode/[id]/index.tsx
@@ -17,6 +17,8 @@ const ResultQRCode = () => {
     githubURL: string
   }
 
+  window.location = "/";
+
   useEffect(() => {
     const fetchUser = async () => {
       if (id) {
@@ -33,10 +35,10 @@ const ResultQRCode = () => {
         <p>Hello, my name is {user?.name}</p>
         <h1>My History</h1>
         <p>Lorem Ipsum is simply dummy text versions of Lorem Ipsum.</p>
-        <Button variant="outlined"><a href={user?.linkedinURL || ""} target="_blank">Linkedin</a></Button>
-        <Button variant="outlined"><a href={user?.githubURL || ""} target="_blank">Github</a></Button>
-        {/* <div className={styles.linkedin} onClick={() => window?.location?.href = user?.linkedinURL}>Linkedin</div>
-        <div className={styles.github} onClick={() => window?.location?.href = user?.githubURL}>Github</div> */}
+        {/* <Button variant="outlined"><a href={user?.linkedinURL || ""} target="_blank">Linkedin</a></Button>
+        <Button variant="outlined"><a href={user?.githubURL || ""} target="_blank">Github</a></Button> */}
+        <div className={styles.linkedin} onClick={() => window?.location?.href = user?.linkedinURL}>Linkedin</div>
+        <div className={styles.github} onClick={() => window?.location?.href = user?.githubURL}>Github</div>
       </div>
 
     </>
